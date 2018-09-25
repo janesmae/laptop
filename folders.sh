6cbccec0ef6e3d1f5a624670200066cb6ee52c04
Jaan Janesmae <git@janesmae.com>
2018-09-25 19:30:36 +0300

    Added symlinks and folder creation into separate scripts

---
+++ folders.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Create folders
+mkdir -p ~/Development/personal
+mkdir -p ~/Development/work
+mkdir -p ~/Screenshots
+
+
---
+++ symlinks.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# Create symlinks
+
+## Zsh
+ln -sfn ~/.files/zsh/zshrc ~/.zshrc
+ln -sfn ~/.files/zsh/ ~/.zsh
+
+## Vim
+ln -sfn ~/.files/vim/vimrc ~/.vimrc
+ln -sfn ~/.files/vim/ ~/.vim
+
+## Git
+ln -sfn ~/.files/git/gitconfig ~/.gitconfig
+ln -sfn ~/.files/git/gitignore ~/.gitignore
+ln -sfn ~/.files/git/gitattributes ~/.gitattributes
+ln -sfn ~/.files/git/gitmessage ~/.gitmessage
+
+## Ansible
+ln -sfn ~/.files/ansible/ansible.cfg ~/.ansible.cfg
+ln -sfn ~/.files/ansible/ ~/.ansible
+
+## SSH
+ln -sfn ~/.files/ssh/ ~/.ssh
+
+## Tmux
+ln -sfn ~/.files/tmux/ ~/.tmux
+ln -sfn ~/.files/tmux/tmux.conf ~/.tmux.conf
