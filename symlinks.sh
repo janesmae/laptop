0dcab775b1081fc577e72130f55100b00b449d16
Jaan Janesmae <git@janesmae.com>
2018-09-25 19:56:36 +0300

    Updated symlinks.sh with tests

--- symlinks.sh
+++ symlinks.sh
@@ -3,26 +3,26 @@
 # Create symlinks
 
 ## Zsh
-ln -sfn ~/.files/zsh/zshrc ~/.zshrc
-ln -sfn ~/.files/zsh/ ~/.zsh
+test -e "${HOME}/.zshrc" || ln -sfn "${HOME}/.files/zsh/zshrc" "${HOME}/.zshrc"
+test -e "${HOME}/.zsh" || ln -sfn "${HOME}/.files/zsh/" "${HOME}/.zsh"
 
 ## Vim
-ln -sfn ~/.files/vim/vimrc ~/.vimrc
-ln -sfn ~/.files/vim/ ~/.vim
+test -e "${HOME}/.vimrc" || ln -sfn "${HOME}/.files/vim/vimrc" "${HOME}/.vimrc"
+test -e "${HOME}/.vim" || ln -sfn "${HOME}/.files/vim/" "${HOME}/.vim"
 
 ## Git
-ln -sfn ~/.files/git/gitconfig ~/.gitconfig
-ln -sfn ~/.files/git/gitignore ~/.gitignore
-ln -sfn ~/.files/git/gitattributes ~/.gitattributes
-ln -sfn ~/.files/git/gitmessage ~/.gitmessage
+test -e "${HOME}/.gitconfig" || ln -sfn "${HOME}/.files/git/gitconfig" "${HOME}/.gitconfig"
+test -e "${HOME}/.gitignore" || ln -sfn "${HOME}/.files/git/gitignore" "${HOME}/.gitignore"
+test -e "${HOME}/.gitattributes" || ln -sfn "${HOME}/.files/git/gitattributes" "${HOME}/.gitattributes"
+test -e "${HOME}/.gitmessage" || ln -sfn "${HOME}/.files/git/gitmessage" "${HOME}/.gitmessage"
 
 ## Ansible
-ln -sfn ~/.files/ansible/ansible.cfg ~/.ansible.cfg
-ln -sfn ~/.files/ansible/ ~/.ansible
+test -e "${HOME}/.ansible.cfg" || ln -sfn "${HOME}/.files/ansible/ansible.cfg" "${HOME}/.ansible.cfg"
+test -e "${HOME}/.ansible" || ln -sfn "${HOME}/.files/ansible/" "${HOME}/.ansible"
 
 ## SSH
-ln -sfn ~/.files/ssh/ ~/.ssh
+test -e "${HOME}/.ssh" || ln -sfn "${HOME}/.files/ssh/" "${HOME}/.ssh"
 
 ## Tmux
-ln -sfn ~/.files/tmux/ ~/.tmux
-ln -sfn ~/.files/tmux/tmux.conf ~/.tmux.conf
+test -e "${HOME}/.tmux.conf" || ln -sfn "${HOME}/.files/tmux/tmux.conf" "${HOME}/.tmux.conf"
+test -e "${HOME}/.tmux" || ln -sfn "${HOME}/.files/tmux/" "${HOME}/.tmux"
