01e9cccf15e9a75de6b4605d358668f0cf41fe59
Jaan Janesmae <git@janesmae.com>
2018-10-16 16:47:24 +0300

    Update macos-settings script

--- macos-settings.sh
+++ macos-settings.sh
@@ -4,7 +4,7 @@ case "$(uname)" in
 	Darwin)
 
 # Ask for computer name
-echo "Please enter the desired hostname [Base]:"
+echo Please enter the desired hostname [Base]:
 read -r HOSTVAR
 
 # Rename the computer
