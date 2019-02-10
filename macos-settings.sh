7f434a1d022d558c31d41726bb615bc91447c92e
Jaan Janesmae <git@janesmae.com>
2019-02-11 00:37:24 +0200

    Update macos settings

--- macos-settings.sh
+++ macos-settings.sh
@@ -4,17 +4,17 @@ case "$(uname)" in
 	Darwin)
 
 # Ask for computer name
-echo Please enter the desired hostname [Base]:
+echo Please enter the desired hostname [j2nesh]:
 read -r HOSTVAR
 
 # Rename the computer
-sudo scutil --set ComputerName "${HOSTVAR:-Base}"
-sudo scutil --set HostName "${HOSTVAR:-Base}"
-sudo scutil --set LocalHostName "${HOSTVAR:-Base}"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${HOSTVAR:-Base}"
+sudo scutil --set ComputerName "${HOSTVAR:-j2nesh}"
+sudo scutil --set HostName "${HOSTVAR:-j2nesh}"
+sudo scutil --set LocalHostName "${HOSTVAR:-j2nesh}"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "${HOSTVAR:-j2nesh}"
 
 # Add message to lock screen
-sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "If found, contact me:\nlostandfound@janesmae.com"
+sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "If found, contact me:\nlost@janesmae.com"
 
 sudo pmset -a standbydelay 86400							# Set a long standby delay
 
