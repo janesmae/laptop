a4642c168bbec9549248ae8172e51a4c55774d62
Jaan Janesmae <git@janesmae.com>
2020-02-25 07:16:58 +0200

    Add aditional folders to installer script

--- folders.sh
+++ folders.sh
@@ -3,6 +3,8 @@
 # Create folders
 mkdir -p "${HOME}/Development/personal"
 mkdir -p "${HOME}/Development/work"
+mkdir -p "${HOME}/Development/public"
+mkdir -p "${HOME}/Development/tmp"
 mkdir -p "${HOME}/Screenshots"
 mkdir -p "${HOME}/bin"
 mkdir -p "${HOME}/tmp"
